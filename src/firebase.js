15446afb2ae84a3c3cc28ba39bcc761567c0418f
Aliyan Karim <162981583+alyan0111@users.noreply.github.com>
2024-04-17 14:14:11 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDS0ZTaJfKdkPbJNmnFvHhAv_53Yy66rMA",
-  authDomain: "my-portfolio-48640.firebaseapp.com",
-  projectId: "my-portfolio-48640",
-  storageBucket: "my-portfolio-48640.appspot.com",
-  messagingSenderId: "128358197792",
-  appId: "1:128358197792:web:27ddd9574a2a5909ba2a45",
-  measurementId: "G-NY3BVXQ26S"
+  apiKey: "yourAPIkey",
+  authDomain: "my-portfolio",
+  projectId: "my-portfolio",
+  storageBucket: "my-portfolio",
+  messagingSenderId: "ID",
+  appId: "ID",
+  measurementId: "Id"
 };
 
 // Initialize Firebase
